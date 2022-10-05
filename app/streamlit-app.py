f08ef025f66f88574d301231b787c8d404ecdcd3
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-05 10:22:20 +0530

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -2,5 +2,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
 
+path = "mdarfan357/price_track/edit/main/app"
+
+os.chdir("../price_track")
 st.write("Hello World")
+st.write(os. getcwd())
