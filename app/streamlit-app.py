652a47dbc56a69379c74b5258c650aa2be4db96b
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-05 12:14:25 +0530

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -3,12 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
-
-path = "mdarfan357/price_track/edit/main/app"
+import seaborn as sns
 
 os.chdir("../price_track")
 st.write("Price Tracker")
-# st.write(os. getcwd())
 data = pd.read_csv("price_track.csv")
 st.write(data)
 st.button("Refresh")
