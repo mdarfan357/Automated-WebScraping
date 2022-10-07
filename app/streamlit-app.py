ce1143dd27e4513645e32a35ffa719b2f2f8e299
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-07 10:12:34 +0530

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -51,7 +51,7 @@ st.write("Please leave a like if you liked the website and share it with your fr
 
 _left, mid, _right = st.columns(3)
 with mid:
-   st.image("https://media0.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy.gif?cid=511e7ad3cmz597iamlotymrf0egpx8ew3xkz592tbj9r40d0&rid=giphy.gif&ct=g",width=1000)
+   st.image("https://media0.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy.gif?cid=511e7ad3cmz597iamlotymrf0egpx8ew3xkz592tbj9r40d0&rid=giphy.gif&ct=g",width=400)
     
 but = st.button("Like")
 if but:
