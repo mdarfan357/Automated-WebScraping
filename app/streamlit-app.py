b4e85139baa7bfbc4606b4f53ccb5503f550ae4b
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-07 10:19:46 +0530

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -52,13 +52,13 @@ with mid:
    st.image("https://media0.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy.gif?cid=511e7ad3cmz597iamlotymrf0egpx8ew3xkz592tbj9r40d0&rid=giphy.gif&ct=g",width=450)
 
 
-st.markdown("<p style='text-align: center;'>Please leave a like if you liked the website and share it with your friends.</p>", unsafe_allow_html=True)
-_left2, mid2, _right2 = st.columns(3)
-with mid2:
-      but = st.button("Like")
-      if but:
-         st.balloons()
-         st.write("Appreciate it :)")
+# st.markdown("<p style='text-align: center;'>Please leave a like if you liked the website and share it with your friends.</p>", unsafe_allow_html=True)
+# _left2, mid2, _right2 = st.columns(3)
+# with mid2:
+#       but = st.button("Like")
+#       if but:
+#          st.balloons()
+#          st.write("Appreciate it :)")
         
 
 
