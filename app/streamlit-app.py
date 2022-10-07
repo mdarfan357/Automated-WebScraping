113675cd55e87d7f641a444f07f0972d954c93ed
Mohammed Arfan <mdarfan357@gmail.com>
2022-10-07 10:03:23 +0530

    Update streamlit-app.py

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 os.chdir("../price_track")
 
-st. markdown("<h1 style='text-align: center;'>Price Tracker</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Price Tracker</h1>", unsafe_allow_html=True)
 st.image("https://editor.analyticsvidhya.com/uploads/80484Thumbnail.png")
 st.write("Here we use data collected from websites to display trends in product prices.")
 st.write("These help people take better descion about the product they wanna buy and the time at which they want to buy the product")
@@ -21,11 +21,11 @@ st.write(data)
 
 
 # st.subheader("")
-st. markdown("<h3 style='text-align: center;'>This is a line chart</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>This is a line chart</h3>", unsafe_allow_html=True)
 st.line_chart(data1)  
-st. markdown("<h3 style='text-align: center;'>This is a area chart</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>This is a area chart</h3>", unsafe_allow_html=True)
 st.area_chart(data1)
-st. markdown("<h3 style='text-align: center;'>This is a bar chart</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>This is a bar chart</h3>", unsafe_allow_html=True)
 st.bar_chart(data1)
 
 st.write()
@@ -33,28 +33,21 @@ latest_amz = int(data1[-1:]["Amazon price"])
 latest_flip = int(data1[-1:]["Flipkart price"])
 
 if latest_amz > latest_flip:
-  st. markdown("<p style='text-align: center;'>Amazon has the least price for the following product.</p>", unsafe_allow_html=True)
+  st.markdown("<p style='text-align: center;'>Amazon has the least price for the following product.</p>", unsafe_allow_html=True)
 if latest_amz > latest_flip:
-  st. markdown("<p style='text-align: center;'>Flipkart has the lowest price for the following product.</p>", unsafe_allow_html=True)
+  st.markdown("<p style='text-align: center;'>Flipkart has the lowest price for the following product.</p>", unsafe_allow_html=True)
 else:
-  st. markdown("<p style='text-align: center;'>Both websites have the same price for the following product.</p>", unsafe_allow_html=True)
+  st.markdown("<p style='text-align: center;'>Both websites have the same price for the following product.</p>", unsafe_allow_html=True)
 st.write("To know more about the next sale check out this [link](https://www.newindianexpress.com/expressdeals/other-categories/amazon-upcoming-sale-in-india/218.html)")
 st.write("Also keep in touch with the official websites [Amazon](https://www.amazon.in/), [Flipkart](https://www.flipkart.com/)")
 
 st.write("--------------")
+
+
+st.markdown("<img style='text-align: center; src="https://media0.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy.gif?cid=511e7ad3cmz597iamlotymrf0egpx8ew3xkz592tbj9r40d0&rid=giphy.gif&ct=g";'></img>", unsafe_allow_html=True)
+# st.image("")
 st.write("Please leave a like if you liked the website and share it with your friends.")
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.write(' ')
-
-with col2:
-    
-    st.image("https://media0.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy.gif?cid=511e7ad3cmz597iamlotymrf0egpx8ew3xkz592tbj9r40d0&rid=giphy.gif&ct=g")
-
-with col3:
-    st.write(' ')
     
 but = st.button("Like")
 if but:
